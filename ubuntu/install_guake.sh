20074e1fc2409cfe2428d7ea092ffab92951042d
reaperes <reaperes105@gmail.com>
2017-06-18 11:35:12 +0900

    add guake

---
+++ ubuntu/install_guake.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# reference: https://github.com/Guake/guake
+
+sudo apt install guake
+# change short cuts
+# toggle F12 to Ctrl+`
+# new tab ctrl+shift+t to ctrl+t
+# close tab ctrl+shift+w to ctrl+w
